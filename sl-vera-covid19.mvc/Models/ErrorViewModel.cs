5caf1225924a0cb2e0e7f3a9aa3299120964e1ae
jb6504 <jb6504@jyskebank.dk>
2020-09-07 08:25:05 +0200

    initial commit

---
+++ sl-vera-covid19.mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sl_vera_covid19.mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
